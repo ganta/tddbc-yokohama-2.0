f7dac1dd240c7c76a6d2d7604f9f93c380daabc8
Hideki IGARASHI <hideki.develop@gmail.com>
2012-09-01 15:40:11 +0900

    [from now] 2012/09/01 15:40:11
    diff --git a/src/main/groovy/tddbc/VendingMachine.groovy b/src/main/groovy/tddbc/VendingMachine.groovy
    index 355cd07..c9b335f 100644
    --- a/src/main/groovy/tddbc/VendingMachine.groovy
    +++ b/src/main/groovy/tddbc/VendingMachine.groovy
    @@ -1,10 +1,24 @@
     package tddbc
    
    +/**
    + * 自動販売機です。
    + */
     class VendingMachine {
    
    -    Integer total = 0
    +    /** 許可するお金 */
         static final List ALLOW_MONEYS = [10, 50, 100, 500, 1000]
    
    +    /** 合計金額 */
    +    Integer total = 0
    +
    +    /**
    +     * お金を投入します。
    +     * <p>
    +     * 許可されないお金が投入された場合はそのまま釣り銭として返します。
    +     *
    +     * @param coin お金
    +     * @return 釣り銭
    +     */
         def insert(Integer coin) {
             if (ALLOW_MONEYS.contains(coin)) {
                 total += coin
    @@ -13,6 +27,13 @@ class VendingMachine {
             coin
         }
    
    +    /**
    +     * 払い戻しをします。
    +     * <p>
    +     * 現在投入されている金額を返し、合計金額を0にします。
    +     *
    +     * @return 釣り銭
    +     */
         def refund() {
             def refund = total
             total = 0

--- src/main/groovy/tddbc/VendingMachine.groovy
+++ src/main/groovy/tddbc/VendingMachine.groovy
@@ -1,10 +1,24 @@
 package tddbc
 
+/**
+ * 自動販売機です。
+ */
 class VendingMachine {
 
-    Integer total = 0
+    /** 許可するお金 */
     static final List ALLOW_MONEYS = [10, 50, 100, 500, 1000]
 
+    /** 合計金額 */
+    Integer total = 0
+
+    /**
+     * お金を投入します。
+     * <p>
+     * 許可されないお金が投入された場合はそのまま釣り銭として返します。
+     *
+     * @param coin お金
+     * @return 釣り銭
+     */
     def insert(Integer coin) {
         if (ALLOW_MONEYS.contains(coin)) {
             total += coin
@@ -13,6 +27,13 @@ class VendingMachine {
         coin
     }
 
+    /**
+     * 払い戻しをします。
+     * <p>
+     * 現在投入されている金額を返し、合計金額を0にします。
+     *
+     * @return 釣り銭
+     */
     def refund() {
         def refund = total
         total = 0
